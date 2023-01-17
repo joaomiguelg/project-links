0e3ee2c29b241e66fb205f2dc593aced66162308
Joao Miguel <jmgonsalves.jmg@gmail.com>
2023-01-17 20:41:13 -0300

    add css animation and media queries

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#perfil-image")
+
+    if (html.classList.contains("light")) {
+        img.setAttribute("src", "./assets/Ellipse.png")
+
+    } else {
+        img.setAttribute("src", "./assets/Ellipse.png")
+    }
+    
+
+    
+}
